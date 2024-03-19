14410c3d979ee928432ced09144de115d8a2eeaa
Kangnam Kim <kangnam7654@naver.com>
2024-03-19 17:12:49 +0900

    changeed model to uncased

--- models/bert.py
+++ models/bert.py
@@ -2,9 +2,9 @@ from transformers import BertModel, BertTokenizer
 
 
 def load_bert(eval=True, device=None):
-    model = BertModel.from_pretrained("bert-base-cased")
-    tokenizer = BertTokenizer.from_pretrained("bert-base-cased")
-    
+    model = BertModel.from_pretrained("bert-base-uncased")
+    tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
+
     if device is not None:
         model = model.to(device)
     if eval:
